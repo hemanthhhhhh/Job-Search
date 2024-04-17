685b59554f08bc392f8f349ce796583bee90a474
hemanthhhhhh <98905333+hemanthhhhhh@users.noreply.github.com>
2024-04-17 08:33:19 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://job-search-backend-production-bcab.up.railway.app/post';
+const API_URL = 'https://job-search-backend-production-bcab.up.railway.app';
 
 export const savePost = async (payload) => {
     try {
